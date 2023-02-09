c9cc731cfc1285a36c8a6c8f4152087b1d9b6634
NadimChibani <npc00@mail.aub.edu>
2023-02-09 10:41:53 +0200

    This phase we introduced queurying ,computed the averages of each transaction type's rate and we returned those values to the api caller

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Ndragon5@localhost:3306/exchange'
 db = SQLAlchemy(app)
 
-#with app.app_context:
-#   db.create_all()
-
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,3 +42,33 @@ def handle_insert():
             category="success",
             status=200
       )
+
+@app.route('/exchangeRate' ,methods=['GET'])
+def handle_Rate_Check():
+    usd_to_lbp_Transactions = Transaction.query.filter_by(usd_to_lbp=1).all()
+    lbp_to_usd_Transactions = Transaction.query.filter_by(usd_to_lbp=0).all()
+
+    usd_to_lbp_Total = 0
+    lbp_to_usd_Total = 0
+
+    for i in usd_to_lbp_Transactions:
+        usd_to_lbp_Total += i.usd_amount/i.lbp_amount 
+
+    for i in lbp_to_usd_Transactions:
+        lbp_to_usd_Total += i.lbp_amount/i.usd_amount 
+
+    #if no data for each case
+    if(len(usd_to_lbp_Transactions)!=0):
+        usd_to_lbp = usd_to_lbp_Total/len(usd_to_lbp_Transactions)
+    else:
+        lbp_to_usd = "NO DATA"   
+
+    if(len(lbp_to_usd_Transactions)!=0):
+        lbp_to_usd = lbp_to_usd_Total/len(lbp_to_usd_Transactions)
+    else:
+        lbp_to_usd = "NO DATA"
+
+    return jsonify(
+        usd_to_lbp = usd_to_lbp,
+        lbp_to_usd = lbp_to_usd
+    )
