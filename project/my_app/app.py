3590089ca4491a9145b5b4a40c10d155bbeed89f
NadimChibani <npc00@mail.aub.edu>
2023-04-30 12:54:57 +0300

    test2

--- project/my_app/app.py
+++ project/my_app/app.py
@@ -10,7 +10,7 @@ import datetime
 from flask import Response
 import json
 from dateutil.relativedelta import relativedelta
-#from project.my_app.db_config import DB_CONFIG
+from project.my_app.db_config import DB_CONFIG
 
 app = Flask(__name__)
 
