4c8d04ded19cfd26929a79e9cceef37be3fc3f32
NadimChibani <npc00@mail.aub.edu>
2023-05-03 16:10:15 +0300

    usertransaction routes implementation

--- project/my_app/services/service_user.py
+++ project/my_app/services/service_user.py
@@ -1,4 +1,6 @@
 from project.my_app.models.user import User
+from project.my_app.services.validator_user import validate_user_id
 
 def get_user(user_id):
+    validate_user_id(user_id)
     return User.query.filter_by(id=user_id).first()
