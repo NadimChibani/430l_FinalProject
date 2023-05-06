1504c6230acb3b34229d20839bfab1d2a7929fa8
NadimChibani <npc00@mail.aub.edu>
2023-05-06 19:08:40 +0300

    fixes

--- project/my_app/blueprints/blueprint_news.py
+++ project/my_app/blueprints/blueprint_news.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 from project.my_app.models.news import News, news_schema, newss_schema
-from project.my_app.services.service_news import handle_number_of_news
+from project.my_app.services.service_news import get_all_news, handle_number_of_news
 from project.my_app.services.service_user import get_user
 from project.my_app.services.validator_user import validate_user_role
 from ..app import add_to_database, get_id_from_authentication, request, jsonify
@@ -21,5 +21,5 @@ def handle_insert():
 
 @blueprint_news.route('/news',methods=['GET'])
 def handle_extract():
-    news = News.query.all()
+    news = get_all_news()
     return jsonify(newss_schema.dump(news)),200
--- project/my_app/models/user.py
+++ project/my_app/models/user.py
@@ -4,6 +4,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(30), unique=True)
     hashed_password = db.Column(db.String(128))
+    user_type = db.Column(db.String(30),nullable=False)
     def __init__(self, user_name, password):
         super(User, self).__init__(user_name=user_name)
         self.hashed_password = bcrypt.generate_password_hash(password)
