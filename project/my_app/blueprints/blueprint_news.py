3628fea1f275bdc0a9cd5f613af59bd6a8382ed1
NadimChibani <npc00@mail.aub.edu>
2023-05-06 11:20:14 +0300

    Implement blueprint for news

---
+++ project/my_app/blueprints/blueprint_news.py
@@ -0,0 +1,25 @@
+from flask import Blueprint
+
+from project.my_app.models.news import News, news_schema, newss_schema
+from project.my_app.services.service_news import handle_number_of_news
+from project.my_app.services.service_user import get_user
+from project.my_app.services.validator_user import validate_user_role
+from ..app import add_to_database, get_id_from_authentication, request, jsonify
+
+blueprint_news = Blueprint(name="blueprint_news", import_name=__name__)
+
+@blueprint_news.route('/news',methods=['POST'])
+def handle_insert():
+    news = request.json["news"]
+    user_id = get_id_from_authentication(request)
+    newscaster_username = validate_user_role("newscaster",user_id).user_name
+
+    handle_number_of_news()
+    new_News = News(newscaster_username,news)
+    add_to_database(new_News)
+    return jsonify(news_schema.dump(new_News)),201
+
+@blueprint_news.route('/news',methods=['GET'])
+def handle_extract():
+    news = News.query.all()
+    return jsonify(newss_schema.dump(news)),200
