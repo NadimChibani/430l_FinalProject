2cdb050a5304aed6d426b2322840409c27e2d60e
NadimChibani <npc00@mail.aub.edu>
2023-05-08 19:35:34 +0300

    fix transaction to mvc

--- project/my_app/storage/storage.py
+++ project/my_app/storage/storage.py
@@ -1,5 +1,7 @@
+import datetime
 from flask import abort
 from project.my_app.app import db
+from project.my_app.models.transaction import Transaction
 from project.my_app.models.user import User
 
 class Storage:
@@ -13,5 +15,20 @@ class Storage:
         except:
             abort(500, 'Error adding to database')
 
+    def get_all_transactions_of_user(user_id):
+        transactions = Transaction.query.filter_by(user_id=user_id).all()
+        return transactions
+
+    def get_all_transactions_ordered_by_date_and_type(usd_to_lbp):
+        transactions = Transaction.query.filter(Transaction.usd_to_lbp == usd_to_lbp).order_by(Transaction.added_date.desc()).all()
+        return transactions
+
+    def get_all_transactions_last_three_days(usd_to_lbp):
+        return Transaction.query.filter(
+            Transaction.added_date.between(datetime.datetime.now() - datetime.timedelta(days=3),datetime.datetime.now())
+            ,Transaction.usd_to_lbp == usd_to_lbp
+            ).all()
+
 def get_user(user_id):
     return User.query.filter_by(id=user_id).first()
+
